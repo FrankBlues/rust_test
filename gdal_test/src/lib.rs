e88310f8267d7ef0d78efebadac4451b614e0c8b
menglimeng <dream_15320@163.com>
2021-09-10 18:50:46 +0800

    update

--- gdal_test/src/lib.rs
+++ gdal_test/src/lib.rs
@@ -1,8 +1,10 @@
 use gdal::raster::{Buffer, GdalType, RasterBand};
-use gdal::{Dataset, Metadata};
+use gdal::{Dataset, Metadata, Driver};
 use gdal_sys::GDALDataType;
 use std::path::Path;
 
+// #[cfg(feature = "ndarray")]
+use ndarray::Array2;
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -69,6 +71,172 @@ mod tests {
             &band_data[pixel_y * cols + pixel_x]
         );
         println!("xy: {:?}", pixel_xy2xy(pixel_x, pixel_y, &gt));
+    }
+
+    #[test]
+    fn test_gdal_raster_read_write() {
+        let in_raster = "/data/read_write_raster/road.tif";
+        let out_raster = "/data/trans_raster.tif";
+
+        let val_value: u8 = 158;
+        let (r, g, b) = (22 as u8, 181 as u8, 255 as u8);
+        let ds = Dataset::open(Path::new(in_raster)).unwrap();
+        let count = ds.raster_count();
+        if count != 1 {
+            println!("Warning: input raster has more than 1 ({}) band", &count);
+            println!("Try using the first band.");
+        }
+        let rb = ds.rasterband(1).unwrap();
+        let (x_size, y_size) = ds.raster_size();
+        let dtype = rb.band_type();
+        println!("{:?}", rb.block_size());
+        println!("{:?}", rb.actual_block_size(((x_size/128) as isize, 0)));
+        println!("{:?}", rb.actual_block_size((0, (y_size/128) as isize)));
+        println!("{:?}", rb.actual_block_size(((x_size/128) as isize, (y_size/128) as isize)));
+        // type T = u8;
+        // match dtype {
+        //     1 => {type T = u8},
+        //     _ => (),
+        // }
+
+        type T= u8;
+
+        let mut rb_data: Buffer<T> = rb.read_band_as().unwrap();
+        
+        // ndarray
+        // let mut arr = Array2::from_shape_vec(
+        //     (y_size, x_size),
+        //     rb_data.data,
+        // ).unwrap();
+        
+        // let mut arr1 = arr.clone();
+        // let mut arr2 = arr.clone();
+        // arr[arr == 158] = 22;
+
+
+        // Output
+        {
+            let driver = Driver::get("GTiff").unwrap();
+            let mut new_ds = driver
+                .create_with_band_type::<T>(
+                    out_raster,
+                    rb.x_size() as isize,
+                    rb.y_size() as isize,
+                    3,
+                )
+                .unwrap();
+            new_ds
+                .set_geo_transform(&ds.geo_transform().unwrap())
+                .unwrap();
+            new_ds.set_spatial_ref(&ds.spatial_ref().unwrap()).unwrap();
+            // band 1
+            
+            // unsafe pointer
+            // let x_ptr = &rb_data.data.as_mut_ptr();
+            // unsafe {
+            //     for i in 0..(x_size * y_size) {
+            //         if *x_ptr.add(i) == val_value {
+            //             *x_ptr.add(i) = r;
+            //         }
+            //     }
+            // }
+
+            // for loop
+            // for d in &mut rb_data.data {
+            //     if *d == val_value {
+            //         *d = r;
+            //     }
+            // }
+
+            // ndarray
+            // arr.mapv_inplace(|x| if x == val_value {r} else {x});
+            // rb_data.data = arr.into_raw_vec();
+
+            // for each
+            &rb_data.data.iter_mut().for_each(|x| {
+                if *x == val_value {
+                    *x = r;
+                }
+            });
+            // rb_data.data = bd;
+            let mut new_rb = new_ds.rasterband(1).unwrap();
+            new_rb.set_no_data_value(0.).unwrap();
+            new_rb
+                .write((0, 0), (x_size, y_size), &rb_data)
+                .unwrap();
+            // band 2
+            // unsafe pointer
+            // unsafe {
+            //     for i in 0..(x_size * y_size) {
+            //         if *x_ptr.add(i) == r {
+            //             *x_ptr.add(i) = g;
+            //         }
+            //     }
+            // }
+
+            // iter.map(|mut x| { if x == r {
+            //     x = g;
+            // }});
+            
+            // arr.mapv_inplace(|x| if x == val_value {r} else {x});
+            // for d in &mut rb_data.data {
+            //     if *d == r {
+            //         *d = g;
+            //     }
+            // }
+            
+            // ndarray
+            // arr1.mapv_inplace(|x| if x == val_value {g} else {x});
+            // rb_data.data = arr1.into_raw_vec();
+
+            // for each
+            &rb_data.data.iter_mut().for_each(|x| {
+                if *x == r {
+                    *x = g;
+                }
+            });
+            let mut new_rb = new_ds.rasterband(2).unwrap();
+            new_rb.set_no_data_value(0.).unwrap();
+            new_rb
+                .write((0, 0), (x_size, y_size), &rb_data)
+                .unwrap();
+
+            // band 3
+            // unsafe pointer
+            // unsafe {
+            //     for i in 0..(x_size * y_size) {
+            //         if *x_ptr.add(i) == g {
+            //             *x_ptr.add(i) = b;
+            //         }
+            //     }
+            // }
+
+            // iter.map(|mut x| { if x == g {
+            //     x = b;
+            // }});
+
+            // for d in &mut rb_data.data {
+            //     if *d == g {
+            //         *d = b;
+            //     }
+            // }
+
+            // ndarray
+            // arr2.mapv_inplace(|x| if x == val_value {b} else {x});
+            // rb_data.data = arr2.into_raw_vec();
+
+            // for each
+            &rb_data.data.iter_mut().for_each(|x| {
+                if *x == g {
+                    *x = b;
+                }
+            });
+            let mut new_rb = new_ds.rasterband(3).unwrap();
+            new_rb.set_no_data_value(0.).unwrap();
+            new_rb
+                .write((0, 0), (x_size, y_size), &rb_data)
+                .unwrap();
+        }
     }
 }
 
--- gdal_test/src/main.rs
+++ gdal_test/src/main.rs
@@ -1,10 +1,8 @@
-use gdal::raster::Buffer;
+use gdal::raster::{Buffer, GdalType};
 use gdal::{Dataset, Driver};
 use std::path::Path;
-use gdal_sys::GDALDataType;
-// #[cfg(feature = "ndarray")]
-use ndarray::Array2;
 
+use ndarray::{Array2, s, Zip};
 fn main() {
     let in_raster = "/data/read_write_raster/road.tif";
     let out_raster = "/data/trans_raster.tif";
@@ -19,7 +17,12 @@ fn main() {
     }
     let rb = ds.rasterband(1).unwrap();
     let (x_size, y_size) = ds.raster_size();
+    println!("{:?}, {:?}", x_size, y_size);
     let dtype = rb.band_type();
+    let (block_x, block_y) = rb.block_size();
+    println!("{:?}", rb.block_size());
+    let (x_remain, _) = rb.actual_block_size(((x_size/128) as isize, 0)).unwrap();
+    let (_, y_remain) = rb.actual_block_size((0, (y_size/128) as isize)).unwrap();
     // type T = u8;
     // match dtype {
     //     1 => {type T = u8},
@@ -28,13 +31,62 @@ fn main() {
 
     type T= u8;
 
-    let mut rb_data: Buffer<T> = rb.read_band_as().unwrap();
-    // let arr = Array2::from_shape_vec(
-    //     (y_size, x_size),
-    //     rb_data.data,
-    // ).unwrap();
-    // arr[arr == 158] = 22;
 
+
+    let mut array = Array2::<u8>::zeros((y_size, x_size));
+    let mut array_g = array.clone();
+    let mut array_b = array.clone();
+
+    for x in 0..=x_size/block_x  {
+        for y in 0..=y_size/block_y {
+            // println!("Reading {}, {} block", x, y);
+            let mut block_arr:Array2::<u8> = rb.read_block((x, y)).unwrap();
+            // block_arr.for_each(|a| {println!("{}", *a)});
+            // println!("{}", block_arr.sum());
+            
+
+            let end_x = if block_x * (x + 1) >= x_size {x_size} else {x*128 + 128};
+            let end_y = if block_y * (y + 1)>= y_size {y_size} else {y*128 + 128};
+            if block_x * (x + 1) >= x_size && !block_y * (y + 1)>= y_size{
+                block_arr = block_arr.slice(s!(0..128, 0..x_remain)).to_owned();
+            } 
+            if block_x * (x + 1) < x_size && block_y * (y + 1)>= y_size {
+                block_arr = block_arr.slice(s!(0..y_remain, 0..128)).to_owned();
+            } 
+            if block_x * (x + 1) >= x_size && block_y * (y + 1)>= y_size {
+                block_arr = block_arr.slice(s!(0..y_remain, 0..x_remain)).to_owned();
+            }
+            
+            let mut slice = array.slice_mut(s!(y*128..end_y, x*128..end_x));
+            let mut slice_g = array_g.slice_mut(s!(y*128..end_y, x*128..end_x));
+            let mut slice_b = array_b.slice_mut(s!(y*128..end_y, x*128..end_x));
+            // break;
+
+            Zip::from(&mut slice).and(&block_arr).for_each(|a, &bb| {
+                if bb == val_value {
+                    *a = r;
+                }
+            });
+            
+            // let block_arr = block_arr.clone();
+            Zip::from(&mut slice_g).and(&block_arr).for_each(|a, &bb| {
+                if bb == val_value {
+                    *a = g;
+                }
+            });
+            
+            Zip::from(&mut slice_b).and(&block_arr).for_each(|a, &bb| {
+                if bb == val_value {
+                    *a = b;
+                }
+            });
+        }
+        // break;
+    }
+
+    let buffer_r = Buffer::new((x_size, y_size), array.into_raw_vec());
+    let buffer_g = Buffer::new((x_size, y_size), array_g.into_raw_vec());
+    let buffer_b = Buffer::new((x_size, y_size), array_b.into_raw_vec());
 
     // Output
     {
@@ -51,40 +103,70 @@ fn main() {
             .set_geo_transform(&ds.geo_transform().unwrap())
             .unwrap();
         new_ds.set_spatial_ref(&ds.spatial_ref().unwrap()).unwrap();
-        // band 1
-        // rb_data.data.into_iter().map(|d| if d == val_value {d = r;});
-        for d in &mut rb_data.data {
-            if *d == val_value {
-                *d = r;
-            }
-        }
+        // for each
+        // &rb_data.data.iter_mut().for_each(|x| {
+        //     if *x == val_value {
+        //         *x = r;
+        //     }
+        // });
+        // rb_data.data = bd;
         let mut new_rb = new_ds.rasterband(1).unwrap();
         new_rb.set_no_data_value(0.).unwrap();
         new_rb
-            .write((0, 0), (x_size, y_size), &rb_data)
+            .write((0, 0), (x_size, y_size), &buffer_r)
             .unwrap();
         // band 2
-        for d in &mut rb_data.data {
-            if *d == r {
-                *d = g;
-            }
-        }
+        // for each
+        // &rb_data.data.iter_mut().for_each(|x| {
+        //     if *x == r {
+        //         *x = g;
+        //     }
+        // });
         let mut new_rb = new_ds.rasterband(2).unwrap();
         new_rb.set_no_data_value(0.).unwrap();
         new_rb
-            .write((0, 0), (x_size, y_size), &rb_data)
+            .write((0, 0), (x_size, y_size), &buffer_g)
             .unwrap();
 
         // band 3
-        for d in &mut rb_data.data {
-            if *d == g {
-                *d = b;
-            }
-        }
+        // for each
+        // &rb_data.data.iter_mut().for_each(|x| {
+        //     if *x == g {
+        //         *x = b;
+        //     }
+        // });
         let mut new_rb = new_ds.rasterband(3).unwrap();
         new_rb.set_no_data_value(0.).unwrap();
         new_rb
-            .write((0, 0), (x_size, y_size), &rb_data)
+            .write((0, 0), (x_size, y_size), &buffer_b)
             .unwrap();
+    }
+
+}
+
+fn create_new_image<T: GdalType + Copy>(out_raster: &str, ds: &gdal::Dataset, buffer: &Buffer<T>) {
+    {
+        let (x_size, y_size) = ds.raster_size();
+        let driver = Driver::get("GTiff").unwrap();
+        let mut new_ds = driver
+            .create_with_band_type::<T>(
+                out_raster,
+                x_size as isize,
+                y_size as isize,
+                3,
+            )
+            .unwrap();
+        new_ds
+            .set_geo_transform(&ds.geo_transform().unwrap())
+            .unwrap();
+        new_ds.set_spatial_ref(&ds.spatial_ref().unwrap()).unwrap();
+
+        for b in 1..4 {
+            let mut new_rb = new_ds.rasterband(b).unwrap();
+            new_rb.set_no_data_value(0.).unwrap();
+            new_rb
+                .write((0, 0), (x_size, y_size), buffer)
+                .unwrap();
+        }
     }
 }
